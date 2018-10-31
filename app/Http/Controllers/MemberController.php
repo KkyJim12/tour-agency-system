7af605a3675d4f477d57caec75789a78d4c1d6b6
KkyJim12 <jirakarnjim1@gmail.com>
2018-10-31 12:42:42 +0700

    make admin script
    make admin

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -35,4 +35,11 @@ class MemberController extends Controller
       $request->session()->regenerate();
       return redirect('/');
     }
+
+    public function RegisterProcess() {
+      $user = new User;
+      $user->user_email = 'admin@royaltour.co.th';
+      $user->user_password = 'admin123';
+      $user->save();
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,8 @@ Route::post('/login-process','MemberController@LoginProcess');
 
 Route::get('/logout-process','MemberController@LogoutProcess');
 
+Route::get('/register-process','MemberController@RegisterProcess');
+
 /* Backend */
 
 Route::middleware(['admin'])->group(function () {
