60ae91b3a7961092207210c7ca9e8a831af259ec
Jimmy <jirakarnjim1@gmail.com>
2019-01-14 03:11:29 +0700

    mobile search
    mobile search

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,12 +1,12 @@
 <div class="mt-5" style="background-color:#f7f7f7; border-top:3px solid #5bc0de">
    <div class="container">
       <div class="row">
-         <div class="col-md-4 mt-5 mb-5">
+         <div class="col-lg-4 mt-5 mb-5">
             <h5>เกี่ยวกับรอยัลทัวร์</h5>
             <p class="mt-4">บริษัท รอยัลทัวร์ แอนด์เทรดดิ้ง จำกัด บริการด้วยความซื่อสัตย์และจริงใจ เจ้าหน้าที่พร้อมให้คำปรึกษากับลูกค้าทุกท่าน เลือกสรรมัคคุเทศก์ที่มีคุณภาพ มีประสบการณ์ มีความรู้ และรักในการบริการ เรายินดีตอบคำถามทุกคำถามเกี่ยวกับการเดินทางหรือโปรแกรมทัวร์ เพื่อความพึงพอใจสูงสุดของลูกค้าทุกท่าน</p>
             <p>เลขใบอนุญาตธุรกิจนำเที่ยว 11/07560</p>
          </div>
-         <div class="col-md-4 mt-5 mb-5">
+         <div class="col-lg-4 mt-5 mb-5">
             <h5>ช่องทางติดต่อ</h5>
             <div class="row mt-4 ml-2">
                <div class="col-2">
@@ -50,7 +50,7 @@
                </div>
             </div>
          </div>
-         <div class="col-md-4 mt-5 mb-5">
+         <div class="col-lg-4 mt-5 mb-5">
             <h5>เข้าร่วมกับเราบน Facebook</h5>
             <div class="fb-page mt-4" data-href="https://www.facebook.com/royaltourgroup/" data-tabs="timeline" data-height="70" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
                <blockquote cite="https://www.facebook.com/royaltourgroup/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/royaltourgroup/">Royal Tour แพคเกจทัวร์เที่ยวต่างประเทศ</a></blockquote>
--- resources/views/components/search-box.blade.php
+++ resources/views/components/search-box.blade.php
@@ -67,3 +67,84 @@
    </div>
 </div>
 <!--End Search Box-->
+
+
+<!-- Mobile Search Box -->
+<p>
+  <a class="btn btn-primary" data-toggle="collapse" href="#mobilesearch" role="button" aria-expanded="false" aria-controls="mobilesearch">
+    Link with href
+  </a>
+</p>
+<div class="collapse" id="mobilesearch">
+  <div class="card card-body">
+    <div class="search-field d-md-block d-lg-none d-xl-none">
+       <div class="container search-box">
+          <div class="row">
+             <div class="col-12">
+                <h3 class='text-left'><img src='/assets/img/filter/worldwide.png' class='icon-title'>ค้นหาทัวร์</h3>
+             </div>
+             <div class="col-12">
+                <div class="tab-content" id="myTabContent">
+                   <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                      <form action="/search-result" method="get">
+                         <div class="row">
+                            <div class="form-group col-12 mb-2">
+                               <label>จุดหมาย</label>
+                               <div class="input-group">
+                                  <div class="input-group-prepend">
+                                     <span class="input-group-text" id="basic-addon1"><i class="fas fa-city"></i></span>
+                                  </div>
+                                  <input type="text" class="form-control" placeholder="ชื่อประเทศ / เมือง" aria-label="Username" name="search_name" aria-describedby="basic-addon1">
+                               </div>
+                            </div>
+                            <div class="form-group col-12 mb-2">
+                               <label>ช่วงเวลาเดินทาง</label>
+                               <div class="input-group">
+                                  <div class="input-group-prepend">
+                                     <span class="input-group-text" id="basic-addon1"><i class="far fa-calendar-alt"></i></span>
+                                  </div>
+                                  <select class="form-control" name="search_tour_month">
+                                     <option value="">เลือกเดือน</option>
+                                     <option value="1">มกราคม</option>
+                                     <option value="2">กุมภาพันธ์</option>
+                                     <option value="3">มีนาคม</option>
+                                     <option value="4">เมษายน</option>
+                                     <option value="5">พฤษภาคม</option>
+                                     <option value="6">มิถุนายน</option>
+                                     <option value="7">กรกฎาคม</option>
+                                     <option value="8">สิงหาคม</option>
+                                     <option value="9">กันยายน</option>
+                                     <option value="10">ตุลาคม</option>
+                                     <option value="11">พฤษจิกายน</option>
+                                     <option value="12">ธันวาคม</option>
+                                  </select>
+                               </div>
+                            </div>
+                            <div class="form-group col-12 mb-4">
+                               <label>รหัสทัวร์</label>
+                               <div class="input-group">
+                                  <div class="input-group-prepend">
+                                     <span class="input-group-text" id="basic-addon1"><i class="fas fa-list-ol"></i></span>
+                                  </div>
+                                  <input class="form-control" type="text" name="search_tour_code" placeholder="รหัสทัวร์">
+                               </div>
+                            </div>
+                            @csrf
+                            <div class="form-group col-12">
+                               <label class="d-none d-sm-none d-md-block d-lg-block">&nbsp</label>
+                               <button class="form-control btn btn-search" type="sumit" name="button"><i class="fas fa-search"></i> Search</button>
+                            </div>
+                         </div>
+                      </form>
+                   </div>
+                   <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
+                   <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
+                </div>
+             </div>
+          </div>
+       </div>
+    </div>
+  </div>
+</div>
+
+<!-- End Mobile Search Box-->
