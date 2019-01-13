691031892c008083372092037cb2e205747cdac4
Jimmy <jirakarnjim1@gmail.com>
2019-01-14 06:39:37 +0700

    test
    test

--- app/Http/Controllers/TourSearchController.php
+++ app/Http/Controllers/TourSearchController.php
@@ -50,10 +50,10 @@ class TourSearchController extends Controller
             $searchConditions[] = ["tour_airline_id", "=", $request->filter_airline];
         }
         if ($minimumPrice >= 0) {
-            $searchConditions[] = [array_map("intval","tour_price[]"), ">=", $minimumPrice];
+            $searchConditions[] = [array_map("intval","tour_price"), ">=", $minimumPrice];
         }
         if ($maximumPrice >= 0 && $maximumPrice > $minimumPrice) {
-            $searchConditions[] = [array_map("intval","tour_price[]"), "<=", $maximumPrice];
+            $searchConditions[] = [array_map("intval","tour_price"), "<=", $maximumPrice];
         }
 
         // Get all matching tours
