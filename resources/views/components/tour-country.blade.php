768e3018344ce8abf70712922ea122d3a77a1fc5
Jimmy <jirakarnjim1@gmail.com>
2019-01-14 02:56:17 +0700

    asd
    asd

--- resources/views/components/tour-country.blade.php
+++ resources/views/components/tour-country.blade.php
@@ -6,13 +6,12 @@
        </div>
     </div>
     <div class="row">
-      <div class="col-12">
         <div class="countryTemp">
           @foreach($continent as $all_continent)
           <h1>{{$all_continent->continent_name}}</h1>
           <div class='row'>
             @foreach($all_continent->subcat as $subcat)
-            <div class="col-12 col-sm-4 col-md-3 col-xl-2 mb-3">
+            <div class="col-6 col-sm-4 col-md-3 col-xl-2 mb-3">
               <a href="/category/{{$subcat->_id}}">
                 <span>
                   <img src='/assets/img/upload/country/{{$subcat->country_img}}'>
@@ -24,7 +23,6 @@
           </div>
           @endforeach
         </div>
-      </div>
     </div>
   </div>
 </div>
