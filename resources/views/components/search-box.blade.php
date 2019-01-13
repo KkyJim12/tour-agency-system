163d1effd741cdbfbf2c59b4462a08d12d478088
Jimmy <jirakarnjim1@gmail.com>
2019-01-14 03:01:54 +0700

    fix123
    fix123

--- resources/views/components/search-box.blade.php
+++ resources/views/components/search-box.blade.php
@@ -1,5 +1,5 @@
 <!--Search Box-->
-<div class="search-field d-none d-sm-none">
+<div class="search-field d-none d-sm-none d-md-block">
    <div class="container search-box">
       <div class="row">
          <div class="col-md-12">
