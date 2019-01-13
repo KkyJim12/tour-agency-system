fff475a43e44794eaf0b81bba5d84e99bfcfb8ed
Jimmy <jirakarnjim1@gmail.com>
2019-01-14 03:48:05 +0700

    asdasd
    asdads

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,6 +12,8 @@
       <link rel="stylesheet" href="/assets/css/thumbnail_image_carousel.css">
       <link rel="stylesheet" href="/assets/css/ion.rangeSlider.css">
       <link rel="stylesheet" href="/assets/css/ion.rangeSlider.skinHTML5.css">
+      <link type="text/css" rel="stylesheet" href="/assets/css/lightslider.css" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/1.3.2/css/lightgallery.css" />
       <link href="https://fonts.googleapis.com/css?family=Kanit:300" rel="stylesheet">
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
       <script src="/assets/js/jquery-3.3.1.min.js" type="text/javascript"></script>
@@ -20,10 +22,8 @@
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
       <script src="/assets/js/jquery.touchSwipe.min.js" type="text/javascript"></script>
       <script src="/assets/js/responsive_bootstrap_carousel.js" type="text/javascript"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/ion-rangeslider/2.3.0/js/ion.rangeSlider.min.js"></script>
+      <script src="/assets/js/ion.rangeSlider.min.js" type="text/javascript"></script>
       <script src="/assets/js/owl.carousel.min.js" type="text/javascript"></script>
-      <link type="text/css" rel="stylesheet" href="/assets/css/lightslider.css" />
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/1.3.2/css/lightgallery.css" />
       <script src="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/1.3.2/js/lightgallery.js"></script>
       <script src="/assets/js/lightslider.js"></script>
       <title>@yield('title')</title>
