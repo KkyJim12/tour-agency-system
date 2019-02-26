cb1c1b36779a850d4209ea1739e2ddb233469512
KkyJim12 <jirakarnjim1@gmail.com>
2019-02-26 08:02:34 +0700

    sss
    dadasd

--- resources/views/components/slide.blade.php
+++ resources/views/components/slide.blade.php
@@ -2,11 +2,11 @@
 <div id="carouselExampleControls" class="carousel slide d-none d-sm-block" data-ride="carousel">
    <div class="carousel-inner main-slide">
       <div class="carousel-item active">
-        <a href="{{$first_slide->slide_link}}"><img class="d-block slide-img" src="/assets/img/upload/slide/{{$first_slide->slide_img}}" alt="First slide" style="width:100%;"></a>
+        <a href="{{$first_slide->slide_link}}"><img class="d-block slide-img" src="/assets/img/upload/slide/{{$first_slide->slide_img}}" alt="First slide"></a>
       </div>
       @foreach($slide as $show_slide)
       <div class="carousel-item">
-         <a href="{{$show_slide->slide_link}}"><img class="d-block slide-img" src="/assets/img/upload/slide/{{$show_slide->slide_img}}" alt="other slide" style="width:100%;"></a>
+         <a href="{{$show_slide->slide_link}}"><img class="d-block slide-img" src="/assets/img/upload/slide/{{$show_slide->slide_img}}" alt="other slide"></a>
       </div>
       @endforeach
    </div>
