90135ec6a114c74a6e52e0f1616e711bd4809349
KkyJim12 <jirakarnjim1@gmail.com>
2019-02-26 07:47:05 +0700

    fix
    fix

--- resources/views/components/slide.blade.php
+++ resources/views/components/slide.blade.php
@@ -2,11 +2,11 @@
 <div id="carouselExampleControls" class="carousel slide d-none d-sm-block" data-ride="carousel">
    <div class="carousel-inner main-slide">
       <div class="carousel-item active">
-        <img class="d-block slide-img" src="/assets/img/upload/slide/{{$first_slide->slide_img}}" alt="First slide">
+        <a href="{{$first_slide->slide_link}}"><img class="d-block slide-img" src="/assets/img/upload/slide/{{$first_slide->slide_img}}" alt="First slide"></a>
       </div>
       @foreach($slide as $show_slide)
       <div class="carousel-item">
-         <img class="d-block slide-img" src="/assets/img/upload/slide/{{$show_slide->slide_img}}" alt="First slide">
+         <a href="{{$show_slide->slide_link}}"><img class="d-block slide-img" src="/assets/img/upload/slide/{{$show_slide->slide_img}}" alt="other slide"></a>
       </div>
       @endforeach
    </div>
